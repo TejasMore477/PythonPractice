82a87bf62e87d7aae0ec721862ccc6d73cadf027
Tejas More <tejasmore660@gmail.com>
2024-04-12 11:07:31 +0530

    implicitTypeConversion

---
+++ JennysToutorials/task2(implicitTypeConversion).py
@@ -0,0 +1,9 @@
+num = input("Enter the number here :")
+text = "what is this"
+print(text + " " + num)
+
+
+num2 = input("Enter the second number :")
+num3 = input("Enter the third number :")
+text2 = input("your name :")
+print(num3 + num2 + text2)
