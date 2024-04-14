5db85ce7ee0de54a57d168bfe888f14fc725906c
Tejas More <tejasmore660@gmail.com>
2024-04-14 11:31:50 +0530

    create and target matrix elem using nested list

---
+++ JennysToutorials/exercise10(targetMatrixLocation).py
@@ -0,0 +1,13 @@
+list1 = ["☠", "☠", "☠"]
+list2 = ["☠", "☠", "☠"]
+list3 = ["☠", "☠", "☠"]
+
+matrix = [list1, list2, list3]
+
+location = input("enter the location to target in the matrix :")
+rowPosition = int(location[0])
+columPosition = int(location[1])
+
+matrix[rowPosition - 1][columPosition - 1] = "X"
+
+print(f"{list1}\n{list2}\n{list3}")
